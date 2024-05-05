e9b6eb30f6974c694cffdf6224a52b813741097d
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2024-05-05 19:24:15 +0200

    dodanie pliku z kodem

---
+++ all_about_cheese.py
@@ -0,0 +1,47 @@
+
+print("Lista zakupów")
+
+goods_list = {
+'motoryzacyjnego': ["części", "kościoły", "błoto"],
+'spozywczego': ['mąkę', 'mleko', 'ser'],
+        # nie mogę 'z' z kropką zrobić
+"drogerii": ["szampon", "proszek", "papugi"],
+"kosmicznego": ['asteroidy', 'galaktyki', 'słońca']
+}
+print("teraz trochę poskładamy values")
+ilosc_moto = len(goods_list['motoryzacyjnego'])
+ilosc_spo = len(goods_list['spozywczego'])
+ilosc_drog = len(goods_list['drogerii'])
+ilosc_kosm = len(goods_list['kosmicznego'])
+ilosc_towaru = ilosc_moto + ilosc_spo + ilosc_drog + ilosc_kosm
+# print(ilosc_towaru)
+
+for shop in goods_list:
+    if shop in goods_list:
+        print(f"Idę do {shop} i kupuję {goods_list[shop]} ")
+
+print(f'Kupuję w sumie {ilosc_towaru} towarów.')
+
+print("teraz będzie inaczej")
+n_o_it = 0
+print("Lista zakupow")
+for shop, items in goods_list.items():
+    print(f"Idę do {shop} i kupuje tam {', '.join(items)}.")
+    n_o_it +=len(items)
+print(f"Kupuję {n_o_it} towarów")
+print("opopopopopoppopoppoppopo")
+print("PALINDROMY ver. 1")
+# cos innego
+
+def czy_palindrom(n):
+   return "tak" if n == n[::-1] else "nie"
+
+
+print(czy_palindrom(input()))
+# wersja 2
+def is_palindrome(string):
+    inverse = string[::-1]
+    if string == inverse:
+        return True
+    else:
+        return False
