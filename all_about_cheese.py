50fc7126b3c791a388600ebfecf905bb5fe75ade
Arkadiusz Pedzisz <serwis.artech@gmail.com>
2024-05-05 21:28:49 +0200

    bez_palin

--- all_about_cheese.py
+++ all_about_cheese.py
@@ -33,15 +33,3 @@ print("opopopopopoppopoppoppopo")
 print("PALINDROMY ver. 1")
 # cos innego
 
-def czy_palindrom(n):
-   return "tak" if n == n[::-1] else "nie"
-
-
-print(czy_palindrom(input()))
-# wersja 2
-def is_palindrome(string):
-    inverse = string[::-1]
-    if string == inverse:
-        return True
-    else:
-        return False
